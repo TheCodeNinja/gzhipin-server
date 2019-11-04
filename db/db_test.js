9f2cef96200047ab4f636ab646b472053c96e51b
TheCodeNinja <hk4public@gmail.com>
2019-11-04 12:45:14 +0800

    Use mongoose to connect db, define schema and model, and save an object

---
+++ db/db_test.js
@@ -0,0 +1,73 @@
+/*
+一. 使用mongoose连接数据库
+1. 引入mongoose
+2. 连接数据库
+3. 获取连接对象
+4. 监听连接
+*/
+
+const md5 = require('blueimp-md5') // 加密的函数
+// 1. 引入mongoose
+const mongoose = require('mongoose')
+// 2. 连接数据库
+mongoose.connect('mongodb://localhost:27017/gzhipin_test', { useNewUrlParser: true })
+// 3. 获取连接对象
+const conn = mongoose.connection
+// 4. 监听连接
+conn.on('connected', function() {
+    console.log('数据库连接成功！')
+})
+conn.on('error',function() {
+    console.log('数据库连接失败！')
+})
+
+/*
+二. 得到对应特定集合的Model
+1. 定义Shema（描述文档结构/document）文档是对象
+2. 定义Model（与集合对应，可以操作集合/collection）集合是数组
+*/
+
+const userSchema = mongoose.Schema({ // 指定文档的结构
+    username: { type: String, required: true }, 
+    password: { type: String, required: true },
+    type: { type: String, required: true }, // Two types: findJob/recruit
+    header: { type: String }
+});
+
+const UserModel = mongoose.model('user', userSchema)
+
+/*
+三. 通过Model或其实例对集合数据进行CRUD操作
+1. 通过Model实例用save()添加数据
+*/
+
+function testSave() {
+    // 创建UserModel的实例
+    // mongoDB自动生成user文档id
+    const userModel = new UserModel({
+        username: 'Mario', 
+        password: md5('pass1234'), 
+        type: 'findJob'
+    })
+    // 调用save()保存
+    userModel.save(function(err, userDoc) {
+        if (err) {
+            console.log(err)
+        }
+        console.log('save()', err, userDoc)
+    })
+}
+
+testSave()
+
+/* 
+OUTPUT: 
+save() null {
+    _id: 5dbfa8f546d8a91ab0bbd01d,
+    username: 'Mario',
+    password: 'b4af804009cb036a4ccdc33431ef9ac9',
+    type: 'findJob',
+    __v: 0
+}
+*/
+
